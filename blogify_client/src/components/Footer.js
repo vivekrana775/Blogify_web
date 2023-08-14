45f0d1c6317b58a0cd92fa9e7eded8a685316a86
vivekrana775 <ranabai775@gmail.com>
2023-08-14 20:37:24 +0530

    Commit

--- blogify_client/src/components/Footer.js
+++ blogify_client/src/components/Footer.js
@@ -2,19 +2,24 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer_container"  >
+    <div className="footer_container">
       <footer className="bg-dark">
-
-        <div style={{
-          color:"white",
-          fontStyle:"oblique",
-
-        }} >Copyright Content :- </div>
-        <div style={{
-          color:"white",
-          fontStyle:"oblique",
-
-        }} >Take Permission from Vivek</div>
+        <div
+          style={{
+            color: "white",
+            fontStyle: "oblique",
+          }}
+        >
+          Copyright Content :-
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontStyle: "oblique",
+          }}
+        >
+          Vivek
+        </div>
       </footer>
     </div>
   );
